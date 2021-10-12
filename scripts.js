3e7844351021788f89b2d4042cf0248caf2cf99e
Jay Yin <jayyin11043@hotmail.com>
2021-10-12 16:42:55 -0400

    fix missing url variable

--- scripts.js
+++ scripts.js
@@ -1,3 +1,3 @@
 console.log(data);
-
+var url = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname + window.location.search
 url.searchParams.set('#', data.num);
