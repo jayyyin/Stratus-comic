4a7749cb4d5f0ec29d0c41af0abe2db979ad44a6
Jay Yin <jayyin11043@hotmail.com>
2021-10-12 17:50:04 -0400

    modifing url

--- scripts.js
+++ scripts.js
@@ -3,16 +3,11 @@ console.log(data);
 var url = new URL('https://xkcd-comics-viewer.herokuapp.com/');
 var search_params = url.searchParams;
 
-// add "topic" parameter
 search_params.set('#', data.num);
-
 url.search = search_params.toString();
-
 var new_url = url.toString();
-
-// output : http://demourl.com/path?id=100&topic=main
 console.log(new_url);
-
+window.location.href = newurl;
 //window.history.replaceState(null, null, "?#=" + data.num);
 //const queryString = window.location.search;
 //console.log(queryString);
