c13e4ddbaa16eedd4bbdca864a22366a57a32753
Jay Yin <jayyin11043@hotmail.com>
2021-10-12 16:07:28 -0400

    saved as data

--- scripts.js
+++ scripts.js
@@ -11,17 +11,7 @@
     };
     xhr.send();
 };*/
-<?php
-$url = 'https://xkcd.com/info.0.json';
-$JSON = file_get_contents($url);
-
-// echo the JSON (you can echo this to JavaScript to use it there)
-echo $JSON;
-
-// You can decode it to process it in PHP
-//$data = json_decode($JSON);
-//var_dump($data);
-?>
+console.log(data)
 
 /*function foo(data)
 {
