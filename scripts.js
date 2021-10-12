cfee671289ea852025981715858b6fa3352f58df
Jay Yin <jayyin11043@hotmail.com>
2021-10-12 17:50:55 -0400

    fixed a derp

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ search_params.set('#', data.num);
 url.search = search_params.toString();
 var new_url = url.toString();
 console.log(new_url);
-window.location.href = newurl;
+window.location.href = new_url;
 //window.history.replaceState(null, null, "?#=" + data.num);
 //const queryString = window.location.search;
 //console.log(queryString);
