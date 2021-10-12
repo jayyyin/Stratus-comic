0bdcd20db188e787c4a2df41cb8d2d90f87e231a
Jay Yin <jayyin11043@hotmail.com>
2021-10-12 18:07:03 -0400

    test2

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ console.log(data);
 
 var url = new URL('https://xkcd-comics-viewer.herokuapp.com/');
 var search_params = url.searchParams;
+console.log(search_params.get('#'));
 if (search_params.get('#') != data.num) {
   search_params.set('#', data.num);
   url.search = search_params.toString();
@@ -9,7 +10,7 @@ if (search_params.get('#') != data.num) {
   console.log(new_url);
   //window.location.href = new_url;
 }
-
+console.log(search_params.get('#'));
 //window.history.replaceState(null, null, "?#=" + data.num);
 //const queryString = window.location.search;
 //console.log(queryString);
