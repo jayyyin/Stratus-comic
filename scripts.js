ab8b1512230bc9bf0b1ceab6153a0c20f408ccfd
Jay Yin <jayyin11043@hotmail.com>
2021-10-12 17:46:19 -0400

    fixed

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
 console.log(data);
+
+var url = new URL('https://xkcd-comics-viewer.herokuapp.com/');
+var search_params = url.searchParams;
+
+// add "topic" parameter
 search_params.set('#', data.num);
+
+url.search = search_params.toString();
+
+var new_url = url.toString();
+
+// output : http://demourl.com/path?id=100&topic=main
+console.log(new_url);
+
 //window.history.replaceState(null, null, "?#=" + data.num);
 //const queryString = window.location.search;
 //console.log(queryString);
