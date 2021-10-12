9c013422637503f92e8d59d52b2711ef9b00b95d
Jay Yin <jayyin11043@hotmail.com>
2021-10-12 16:33:31 -0400

    removed junk code

--- index.php
+++ index.php
@@ -14,15 +14,9 @@
   var data = <?php
   $url = 'https://xkcd.com/info.0.json';
   $JSON = file_get_contents($url);
-
-  // echo the JSON (you can echo this to JavaScript to use it there)
   echo $JSON;
-
-  // You can decode it to process it in PHP
-  //$data = json_decode($JSON);
-  //var_dump($data);
   ?>;
-  
+
 </script>
 <script src="scripts.js"></script>
 <h1>This is a Heading</h1>
