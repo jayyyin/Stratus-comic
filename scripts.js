31890ad539241295f94bf89f10bc0b250045618b
Jay Yin <jayyin11043@hotmail.com>
2021-10-12 16:03:38 -0400

    testing php

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-var getJSON = function(url, callback) {
+/*var getJSON = function(url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
-    xhr.responseType = 'json';
+    xhr.responseType = 'jsonp';
     xhr.onreadystatechange = function() {
       if (xhr.readState === 4) {
         callback(null, xhr.response);
@@ -10,8 +10,35 @@ var getJSON = function(url, callback) {
       }
     };
     xhr.send();
-};
+};*/
 
+
+/*function foo(data)
+{
+    console.log(data.num);
+}
+
+var script = document.createElement('script');
+script.src = 'https://xkcd.com/info.0.json'
+script.type = 'application/jsonp'
+document.head.appendChild(script);
+
+console.log(script);*/
+// or document.head.appendChild(script) in modern browsers
+
+/*
+var request = new Request('https://xkcd.com/info.0.json');
+
+fetch(request, {mode: 'no-cors'}).then(function(response) {
+    // Convert to JSON
+    return response.json();
+}).then(function(j) {
+    // Yay, `j` is a JavaScript object
+    console.log(JSON.stringify(j));
+}).catch(function(error) {
+    console.log('Request failed', error)
+});*/
+/*
 getJSON('https://xkcd.com/info.0.json',
 function(err, data) {
   if (err !== null) {
@@ -19,4 +46,4 @@ function(err, data) {
   } else {
     alert('Your query count: ' + data.query.count);
   }
-});
+});*/
