34e70c6b49fb8f0f8c1de2567b80a4897c4dba48
Jay Yin <jayyin11043@hotmail.com>
2021-10-12 17:33:14 -0400

    test

--- index.php
+++ index.php
@@ -12,22 +12,17 @@
 
   <script>
   var data = <?php
-  $gotNumber;
   if (isset($_GET['#'])) {
-    $gotNumber = False;
     echo $_GET['#'];
     $url = 'https://xkcd.com/' . $_GET['#'] . '/info.0.json';
   } else {
-    $gotNumber = True;
     $url = 'https://xkcd.com/info.0.json';
 
   }
   $JSON = file_get_contents($url);
   echo $JSON;
-  if(gotNumber){
-    $url .= (parse_url($url, PHP_URL_QUERY) ? '&' : '?') . '#=' . JSON['num'];
-  }
-  ?>;
+  
+  ?>
 
 </script>
 <script src="scripts.js"></script>
