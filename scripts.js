3d89d1f2de3b0c50b3ac243092b6ffa1087c3fd9
Jay Yin <jayyin11043@hotmail.com>
2021-10-12 14:05:39 -0400

    First files and API JSON retreval testing

---
+++ scripts.js
@@ -0,0 +1,23 @@
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onreadystatechange = function() { if (xhr.readState === 4) {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
+
+getJSON('https://xkcd.com/info.0.json',
+function(err, data) {
+  if (err !== null) {
+    alert('Something went wrong: ' + err);
+  } else {
+    alert('Your query count: ' + data.query.count);
+  }
+});
