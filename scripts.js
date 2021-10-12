b7adf68b175f3acfeffbd0166a5c2c0a4117970e
Jay Yin <jayyin11043@hotmail.com>
2021-10-12 17:28:37 -0400

    commented out test code

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 console.log(data);
 //window.history.replaceState(null, null, "?#=" + data.num);
-const queryString = window.location.search;
-console.log(queryString);
+//const queryString = window.location.search;
+//console.log(queryString);
