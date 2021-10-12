7f19816b90e5035ef15acb4ed1123063e5d87948
Jay Yin <jayyin11043@hotmail.com>
2021-10-12 16:47:44 -0400

    changed url update method

--- scripts.js
+++ scripts.js
@@ -1,3 +1,2 @@
 console.log(data);
-var url = new URL(window.location.protocol + "//" + window.location.host + "/" + window.location.pathname + window.location.search)
-url.searchParams.set('#', data.num);
+window.history.replaceState(null, null, "?#=" + data.num);
