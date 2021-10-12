22dc76b7d7f56940a61ca3061b7cccdb6ba5c8a1
Jay Yin <jayyin11043@hotmail.com>
2021-10-12 18:08:16 -0400

    possibly solved infinite refresh issue

--- scripts.js
+++ scripts.js
@@ -2,15 +2,15 @@ console.log(data);
 
 var url = new URL('https://xkcd-comics-viewer.herokuapp.com/');
 var search_params = url.searchParams;
-console.log(search_params.get('#'));
-if (search_params.get('#') != data.num) {
+//console.log(search_params.get('#'));
+if (search_params.get('#') !== data.num) {
   search_params.set('#', data.num);
   url.search = search_params.toString();
   var new_url = url.toString();
   console.log(new_url);
-  //window.location.href = new_url;
+  window.location.href = new_url;
 }
-console.log(search_params.get('#'));
+//console.log(search_params.get('#'));
 //window.history.replaceState(null, null, "?#=" + data.num);
 //const queryString = window.location.search;
 //console.log(queryString);
