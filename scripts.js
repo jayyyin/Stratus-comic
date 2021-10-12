3df002cbbc48182a5e25c4d0ac2dedd3062d2101
Jay Yin <jayyin11043@hotmail.com>
2021-10-12 18:02:09 -0400

    fixed infinite loop

--- scripts.js
+++ scripts.js
@@ -2,12 +2,14 @@ console.log(data);
 
 var url = new URL('https://xkcd-comics-viewer.herokuapp.com/');
 var search_params = url.searchParams;
+if (search_params.get('#') != data.num) {
+  search_params.set('#', data.num);
+  url.search = search_params.toString();
+  var new_url = url.toString();
+  console.log(new_url);
+  window.location.href = new_url;
+}
 
-search_params.set('#', data.num);
-url.search = search_params.toString();
-var new_url = url.toString();
-console.log(new_url);
-window.location.href = new_url;
 //window.history.replaceState(null, null, "?#=" + data.num);
 //const queryString = window.location.search;
 //console.log(queryString);
