0fab96ab8c8dc64d66c0a224fc86be5a8c6259c0
Jay Yin <jayyin11043@hotmail.com>
2021-10-12 18:06:32 -0400

    testing

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ if (search_params.get('#') != data.num) {
   url.search = search_params.toString();
   var new_url = url.toString();
   console.log(new_url);
-  window.location.href = new_url;
+  //window.location.href = new_url;
 }
 
 //window.history.replaceState(null, null, "?#=" + data.num);
