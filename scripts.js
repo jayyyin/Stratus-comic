6a06845c77b9614f72802f8e0109cb9ce2d11292
Jay Yin <jayyin11043@hotmail.com>
2021-10-12 16:05:40 -0400

    reverted change

--- scripts.js
+++ scripts.js
@@ -11,7 +11,17 @@
     };
     xhr.send();
 };*/
+<?php
+$url = 'https://xkcd.com/info.0.json';
+$JSON = file_get_contents($url);
 
+// echo the JSON (you can echo this to JavaScript to use it there)
+echo $JSON;
+
+// You can decode it to process it in PHP
+//$data = json_decode($JSON);
+//var_dump($data);
+?>
 
 /*function foo(data)
 {
