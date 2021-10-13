9209eef3134f5605c322231a06e7e5fcf63a2ec5
Jay Yin <jayyin11043@hotmail.com>
2021-10-13 14:52:33 -0400

    changed dataLoad to be a function

--- init.js
+++ init.js
@@ -1,18 +1,24 @@
 console.log(data);
+function loadData(init) {
+  var url = new URL('https://xkcd-comics-viewer.herokuapp.com/');
+  var search_params = url.searchParams;
+  //console.log(search_params.get('#'));
+  if (search_params.get('#') !== data.num) {
+    search_params.set('#', data.num);
+    url.search = search_params.toString();
+    var new_url = url.toString();
+    console.log(new_url);
+    if (init) {
+      window.history.replaceState(null, null, "?%23=" + data.num);
+    }
+    else {
+      window.location.href = new_url;
+    }
+    //window.location.href = new_url;
+  };
+};
 
-var url = new URL('https://xkcd-comics-viewer.herokuapp.com/');
-var search_params = url.searchParams;
-//console.log(search_params.get('#'));
-if (search_params.get('#') !== data.num) {
-  search_params.set('#', data.num);
-  url.search = search_params.toString();
-  var new_url = url.toString();
-  console.log(new_url);
-  window.history.replaceState(null, null, "?%23=" + data.num);
-  //window.location.href = new_url;
-}
-
-
+loadData(True);
 //console.log(search_params.get('#'));
 //window.history.replaceState(null, null, "?#=" + data.num);
 //const queryString = window.location.search;
