e398cc8b7567e9dd8a199631f9dff906f7534525
Jay Yin <jayyin11043@hotmail.com>
2021-10-13 14:47:57 -0400

    added some brackets

--- load.js
+++ load.js
@@ -1,6 +1,6 @@
 document.getElementById("comic").src = data.img;
 
 document.getElementById("prevButton").addEventListener("click", function() {
-  search_params.set('#', data.num-1);
-  window.history.replaceState(null, null, "?%23=" + data.num-1);
+  search_params.set('#', (data.num-1));
+  window.history.replaceState(null, null, "?%23=" + (data.num-1));
 });
