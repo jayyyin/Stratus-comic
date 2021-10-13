fed7639696515043aee7d20c506ef4420cda187d
Jay Yin <jayyin11043@hotmail.com>
2021-10-13 16:41:21 -0400

    fancy string manipulation

--- load.js
+++ load.js
@@ -15,6 +15,24 @@ var dateString = ''+ data.year + '-' + data.month + '-' + data.day
 const creationDate = new Date(dateString)
 var date = document.getElementById("date").textContent = creationDate.toLocaleString('default', {month: 'long', day: '2-digit', year: 'numeric' });
 if (data.transcript !== "") {
-  var stringedTranscript = data.transcript.replaceAll( '\n', '<br>')
+  //replace all new lines with break rows
+  var stringedTranscript = data.transcript.replaceAll( '\n', '<br>');
+
+
+  //finding the title string
+  var indx1 = stringedTranscript.indexOf("{") + 1;
+  var indx2 = stringedTranscript.lastIndexOf("}");
+  var subString = stringedTranscript.substring(idx1,idx2);
+
+  //removing the title string from the original transcript
+  stringedTranscript = stringedTranscript.substring(0, indx1-1);
+
+  //removing "title string" and curly braces
+  var subString2 = subString.substring( subString.indexOf(':') + 1,
+    subString.indexOf('}')
+  );
+
+  //reinserting title string back in as a title
+  stringedTranscript = "<h3>" + subString2 + "</h3><br>" + stringedTranscript;
   document.getElementById("transcript").innerHTML = stringedTranscript;
 }
