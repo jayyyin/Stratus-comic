a746b57c10a22634a389c9c5077d13d631cbda51
Jay Yin <jayyin11043@hotmail.com>
2021-10-14 16:40:24 -0400

    testing out unescaped slashes

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
       $url = 'https://xkcd.com/info.0.json';
       $JSON = file_get_contents($url);
     }
-    echo json_decode(json_encode($JSON));
+    echo json_decode(json_encode($JSON, JSON_UNESCAPED_SLASHES));
     ?>');
 
     </script>
