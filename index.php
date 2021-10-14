8e726993a2f2df6565cdf25fd5a0362469aa35f3
Jay Yin <jayyin11043@hotmail.com>
2021-10-14 16:48:35 -0400

    yet another test

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
       $url = 'https://xkcd.com/info.0.json';
       $JSON = file_get_contents($url);
     }
-    echo json_decode(json_encode(stripslashes($JSON), JSON_UNESCAPED_SLASHES)));
+    echo json_decode(stripslashes($JSON));
     ?>');
 
     </script>
